cfa40d60cf48c54babf1efc29fa27a4ede395c22
Federico Bonarrigo <federico.bonarrigo@apparound.com>
2024-12-03 00:56:37 +0100

    first 2 days

---
+++ Day01.py
@@ -0,0 +1,35 @@
+# Day 1 advent of code 2024
+def P1(f):
+    list1 = []
+    list2 = []
+    distance = 0
+    for x in f:
+        row = x.split()
+        list1.append(int(row[0]))
+        list2.append(int(row[1]))
+    list1.sort()
+    list2.sort()
+    for i in range(len(list1)):
+        distance += abs(list1[i] - list2[i])
+    return distance
+
+
+def P2(f):
+    list1 = []
+    list2 = []
+    similarity = 0
+    for x in f:
+        row = x.split()
+        list1.append(int(row[0]))
+        list2.append(int(row[1]))
+    for x in list1:
+        similarity += (x * list2.count(x))
+    return similarity
+
+
+f = open("resources\day01.txt", "r")
+print(f"Day 1/1: {P1(f)}")
+f.close()
+f = open("resources\day01.txt", "r")
+print(f"Day 1/2: {P2(f)}")
+f.close()
---
+++ Day02.py
@@ -0,0 +1,49 @@
+# day 2 advent of code 2024
+def P1(f):
+    safes = []
+    for x in f:
+        report = [int(v) for v in x.split()]
+        safes.append(IsSafe(report))
+    return sum(safes)
+
+
+def P2(f):
+    safes = 0
+    for x in f:
+        report = [int(v) for v in x.split()]
+        safe = IsSafe(report)
+        if safe == 1:
+            safes += 1
+            continue
+        for i in range(len(report)):
+            report2 = report.copy()
+            report2.pop(i)
+            if IsSafe(report2):
+                safes += 1
+                break
+    return safes
+
+# Return 0 if not safe or 1 if safe, so I can sum the results
+
+
+def IsSafe(report):
+    for i in range(len(report)-1):
+        diff = report[i+1] - report[i]
+        if abs(diff) > 3 or abs(diff) == 0:
+            return 0  # not safe
+        if i == 0:
+            ascending = diff > 0
+            continue
+        if diff < 0 and ascending:
+            return 0
+        if diff > 0 and not ascending:
+            return 0
+    return 1
+
+
+f = open("resources\day02.txt", "r")
+print(f"Day 2/1: {P1(f)}")
+f.close()
+f = open("resources\day02.txt", "r")
+print(f"Day 2/2: {P2(f)}")
+f.close()
